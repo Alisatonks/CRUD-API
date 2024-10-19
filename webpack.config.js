ae572a034a7c7ff388873a45b060e6d7813fe510
Volha Haiduk <Alisatonks2385@gmail.com>
2024-10-19 15:34:52 +0200

    fix: change configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
-import path from 'path';
-
-const isProduction = process.env.NODE_ENV === 'production';
-
-export default {
-  entry: './src/index.ts', 
-  target: 'node', 
-  mode: isProduction ? 'production' : 'development',
-  module: {
-    rules: [
-        {
-            test: /\.ts$/,
-            use: 'ts-loader', 
-            exclude: /node_modules/,
-        },
-    ],
-},
-  output: {
-    filename: 'bundle.js', 
-    path: path.resolve(process.cwd(), 'dist'),
+module.exports = {
+    entry: './src/index.ts',
+    target: 'node', 
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/, 
+                use: ['ts-loader'],
+                exclude: /node_modules/
+            }
+        ]
+    },
+    output: {
+      filename: 'index.js',
+      path: require('path').resolve(__dirname, 'dist'), 
+      clean: true 
   },
   resolve: {
-    extensions: ['.ts', '.js'],
+      extensions: ['.ts', '.js'] 
+  },
+  optimization: {
+    minimize: true,
   },
 };
